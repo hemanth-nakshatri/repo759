7a934868692edbb63563f8cdf7d890ff011df7b3
hemanth-nakshatri <hemans992@gmail.com>
2024-09-15 14:04:34 -0500

    Update task4.sh to include partition

--- HW01/task4.sh
+++ HW01/task4.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/usr/bin/env zsh
 
 #SBATCH --job-name=FirstSlurm       # Job name
-#SBATCH --ntasks=1                  # Run on a single CPU task
 #SBATCH --cpus-per-task=2           # Ask for 2 CPU cores
 #SBATCH --output=FirstSlurm.out      # Standard output file
 #SBATCH --error=FirstSlurm.err       # Standard error file
+#SBATCH --partition=instruction
 
 echo $SLURMD_NODENAME
