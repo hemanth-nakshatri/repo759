a70c20949972e43057d8b5ec418608b95ac5a87a
hemanth-nakshatri <hemans992@gmail.com>
2024-09-22 19:25:50 -0500

    Add gitignore

--- HW02/task1.sh
+++ HW02/task1.sh
@@ -13,8 +13,8 @@ g++ scan.cpp task1.cpp -Wall -O3 -std=c++17 -o task1
 # Loop through values of n from 2^10 to 2^30
 for exp in {10..30}; do
     n=$((2**exp)) # Calculate n as 2^exp
-    echo "Running task1 with n=$n"
+    echo "Running task1 with exp=$exp"
     
     # Run the program with n and append the results to a file
-    ./task1 $n >> scaling_results.txt >> ","
+    ./task1 $n >> scaling_results.txt 
 done
